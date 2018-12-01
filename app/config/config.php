9e9dd4ed70d031768a69c5dcbb7de25eb2832058
Dave Sayer <hello@crafted.im>
2018-12-01 01:54:11 +0000

    staging url

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.33.10';
+$config['base_url'] = 'tictactoecrafted.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
