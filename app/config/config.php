cfeaa669b227c8cd6891d138585fc2441ddcda11
Dave Sayer <hello@crafted.im>
2018-12-01 10:52:47 +0000

    properly setting base_url

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'tictactoecrafted.herokuapp.com';
+$config['base_url'] = 'https://tictactoecrafted.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
